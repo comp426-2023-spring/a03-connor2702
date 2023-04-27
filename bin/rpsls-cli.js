703bc83054002aee119660234edca48935c37c17
root <root@UNC-PF2A3Z3S>
2023-04-27 15:47:32 -0400

    Added help and rules

--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -1,1 +1,38 @@
 #!/usr/bin/env node
+
+import minimist from "minimist";
+import { rpsls } from "/lib/rpsls.js";
+
+const argument = minimist(process.argv.slice(2));
+
+if (argument.h || argument.help) {
+    console.log(`
+    Usage: node-rpsls [SHOT]
+    Play the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!
+    
+      -h, --help        display this help message and exit
+      -r, --rules       display the rules and exit
+    
+    Examples:
+      node-rpsls        Return JSON with single player RPSLS result.
+                        e.g. {"player":"rock"}
+      node-rpsls rock   Return JSON with results for RPSLS played against a simulated opponent.
+                        e.g {"player":"rock","opponent":"Spock","result":"lose"}`);
+}
+
+if (argument.r || argument.rules) {
+    console.log(`
+    Rules for the Lizard-Spock Espansion of Rock Paper Scissors:
+
+  - Scissors CUTS Paper
+  - Paper COVERS Rock
+  - Rock SMOOSHES Lizard
+  - Lizard POISONS Spock
+  - Spock SMASHES Scissors
+  - Scissors DECAPITATES Lizard
+  - Lizard EATS Paper
+  - Paper DISPROVES Spock
+  - Spock VAPORIZES Rock
+  - Rock CRUSHES Scissors
+    `);
+}
