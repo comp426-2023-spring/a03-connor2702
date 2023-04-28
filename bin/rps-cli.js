cba85d8fb756c4afd58da6064c35aa49f690cf48
root <root@UNC-PF2A3Z3S>
2023-04-27 20:56:05 -0400

    Finished rps attempt 1

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -33,3 +33,28 @@ if (argument.r || argument.rules) {
     process.exit(0);
 }
 
+try {
+    console.log(JSON.stringify(rps(argument._[0])));
+} catch (error) {
+    if (error instanceof RangeError) {
+        console.log(`
+        Usage: node-rps [SHOT]
+        Play Rock Paper Scissors (RPS)
+        
+          -h, --help      display this help message and exit
+          -r, --rules     display the rules and exit
+        
+        Examples:
+          node-rps        Return JSON with single player RPS result.
+                          e.g. {"player":"rock"}
+          node-rps rock   Return JSON with results for RPS played against a simulated opponent.
+                          e.g {"player":"rock","opponent":"scissors","result":"win"}`);
+        console.log(`
+        Rules for Rock Paper Scissors:
+    
+        - Scissors CUTS Paper
+        - Paper COVERS Rock
+        - Rock CRUSHES Scissors`);
+        process.exit(1);
+    }
+}
